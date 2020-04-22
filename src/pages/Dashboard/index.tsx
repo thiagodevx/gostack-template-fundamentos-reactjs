62a576b66469b4c50bb21f24390cf9f8fa29cca0
thiagodevx <thiago.dev.x@gmail.com>
2020-04-22 04:18:11 -0300

    primeiros passos do projeto

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,45 +1,49 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
-import income from '../../assets/income.svg';
-import outcome from '../../assets/outcome.svg';
-import total from '../../assets/total.svg';
+import income from '../../assets/income.svg'
+import outcome from '../../assets/outcome.svg'
+import total from '../../assets/total.svg'
 
-import api from '../../services/api';
+import api from '../../services/api'
 
-import Header from '../../components/Header';
+import Header from '../../components/Header'
 
-import formatValue from '../../utils/formatValue';
+import formatValue from '../../utils/formatValue'
 
-import { Container, CardContainer, Card, TableContainer } from './styles';
+import { Container, CardContainer, Card, TableContainer } from './styles'
 
 interface Transaction {
-  id: string;
-  title: string;
-  value: number;
-  formattedValue: string;
-  formattedDate: string;
-  type: 'income' | 'outcome';
-  category: { title: string };
-  created_at: Date;
+  id: string
+  title: string
+  value: number
+  formattedValue: string
+  formattedDate: string
+  type: 'income' | 'outcome'
+  category: { title: string }
+  created_at: Date
 }
 
 interface Balance {
-  income: string;
-  outcome: string;
-  total: string;
+  income: string
+  outcome: string
+  total: string
+}
+
+interface TransactionsResponse {
+  transactions: Transaction[]
+  balance: Balance
 }
 
 const Dashboard: React.FC = () => {
-  // const [transactions, setTransactions] = useState<Transaction[]>([]);
-  // const [balance, setBalance] = useState<Balance>({} as Balance);
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+  const [balance, setBalance] = useState<Balance>({} as Balance)
 
   useEffect(() => {
-    async function loadTransactions(): Promise<void> {
-      // TODO
-    }
-
-    loadTransactions();
-  }, []);
+    api.get<TransactionsResponse>('transactions').then(response => {
+      setTransactions(response.data.transactions)
+      setBalance(response.data.balance)
+    })
+  }, [])
 
   return (
     <>
@@ -49,23 +53,23 @@ const Dashboard: React.FC = () => {
           <Card>
             <header>
               <p>Entradas</p>
-              <img src={income} alt="Income" />
+              <img src={income} alt='Income' />
             </header>
-            <h1 data-testid="balance-income">R$ 5.000,00</h1>
+            <h1 data-testid='balance-income'>R$ {balance.income}</h1>
           </Card>
           <Card>
             <header>
               <p>Saídas</p>
-              <img src={outcome} alt="Outcome" />
+              <img src={outcome} alt='Outcome' />
             </header>
-            <h1 data-testid="balance-outcome">R$ 1.000,00</h1>
+            <h1 data-testid='balance-outcome'>R$ {balance.outcome}</h1>
           </Card>
           <Card total>
             <header>
               <p>Total</p>
-              <img src={total} alt="Total" />
+              <img src={total} alt='Total' />
             </header>
-            <h1 data-testid="balance-total">R$ 4000,00</h1>
+            <h1 data-testid='balance-total'>R$ {balance.total}</h1>
           </Card>
         </CardContainer>
 
@@ -81,24 +85,20 @@ const Dashboard: React.FC = () => {
             </thead>
 
             <tbody>
-              <tr>
-                <td className="title">Computer</td>
-                <td className="income">R$ 5.000,00</td>
-                <td>Sell</td>
-                <td>20/04/2020</td>
-              </tr>
-              <tr>
-                <td className="title">Website Hosting</td>
-                <td className="outcome">- R$ 1.000,00</td>
-                <td>Hosting</td>
-                <td>19/04/2020</td>
-              </tr>
+              {transactions.map(transaction => (
+                <tr key={transaction.id}>
+                  <td className='title'>{transaction.title}</td>
+                  <td className={transaction.type}>R$ {transaction.value}</td>
+                  <td>{transaction.category.title}</td>
+                  <td>{transaction.formattedDate}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </TableContainer>
       </Container>
     </>
-  );
-};
+  )
+}
 
-export default Dashboard;
+export default Dashboard
